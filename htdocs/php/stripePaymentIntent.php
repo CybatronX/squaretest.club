272cca9e2b33ea44c171c53cd304da488de6bc65
Ajay <ajaydarez@gmail.com>
2018-12-23 11:24:17 -0800

    another commit

--- htdocs/php/stripePaymentIntent.php
+++ htdocs/php/stripePaymentIntent.php
@@ -1,24 +1,25 @@
+<HTML>
 <?php
   require_once '/var/secrets/stripeTestKey.php';
   
-  echo $key;
- // 	\Stripe\Stripe::setApiKey($key);
+ 	\Stripe\Stripe::setApiKey($key);
 
-	// \Stripe\PaymentIntent::create([
-	//   "amount" => 100,
-	//   "currency" => "eur",
-	//   "allowed_source_types" => ["card"],
-	// ]);
+	$intent = \Stripe\PaymentIntent::create([
+	  "amount" => 100,
+	  "currency" => "eur",
+	  "allowed_source_types" => ["card"],
+	]);
+	echo $intent->client_secret;
 ?>
 
-<HTML>
+
 
 <body>
-<!-- 	<input id="cardholder-name" type="text">
+	<input id="cardholder-name" type="text">
 	<div id="card-element"></div>
 	<button id="card-button" data-secret="<?= $intent->client_secret ?>">
 	  Submit Payment
-	</button> -->
+	</button>
 </body>
 
 </HTML>
