5eb02c6f1d97d493f8178f584a0d2b343a885388
Ajay <ajaydarez@gmail.com>
2018-12-23 10:08:27 -0800

    another commit

--- htdocs/php/squareCheckout.php
+++ htdocs/php/squareCheckout.php
@@ -5,55 +5,55 @@
 
 	echo $locationId;
 
-// Create and configure a new API client object
-	$defaultApiConfig = new \SquareConnect\Configuration();
-	$defaultApiConfig->setAccessToken($accessToken);
-	$defaultApiClient = new \SquareConnect\ApiClient($defaultApiConfig);
-	$checkoutClient = new SquareConnect\Api\CheckoutApi($defaultApiClient);
+// // Create and configure a new API client object
+// 	$defaultApiConfig = new \SquareConnect\Configuration();
+// 	$defaultApiConfig->setAccessToken($accessToken);
+// 	$defaultApiClient = new \SquareConnect\ApiClient($defaultApiConfig);
+// 	$checkoutClient = new SquareConnect\Api\CheckoutApi($defaultApiClient);
 
-	//Create a Money object to represent the price of the line item.
-	$price = new \SquareConnect\Model\Money;
-	$price->setAmount(600);
-	$price->setCurrency('USD');
+// 	//Create a Money object to represent the price of the line item.
+// 	$price = new \SquareConnect\Model\Money;
+// 	$price->setAmount(600);
+// 	$price->setCurrency('USD');
 
-	//Create the line item and set details
-	$book = new \SquareConnect\Model\CreateOrderRequestLineItem;
-	$book->setName('The Shining');
-	$book->setQuantity('2');
-	$book->setBasePriceMoney($price);
+// 	//Create the line item and set details
+// 	$book = new \SquareConnect\Model\CreateOrderRequestLineItem;
+// 	$book->setName('The Shining');
+// 	$book->setQuantity('2');
+// 	$book->setBasePriceMoney($price);
 
-	//Puts our line item object in an array called lineItems.
-	$lineItems = array();
-	array_push($lineItems, $book);
+// 	//Puts our line item object in an array called lineItems.
+// 	$lineItems = array();
+// 	array_push($lineItems, $book);
 
-	// Create an Order object using line items from above
-	$order = new \SquareConnect\Model\CreateOrderRequest();
+// 	// Create an Order object using line items from above
+// 	$order = new \SquareConnect\Model\CreateOrderRequest();
 
-	$order->setIdempotencyKey(uniqid()); //uniqid() generates a random string.
+// 	$order->setIdempotencyKey(uniqid()); //uniqid() generates a random string.
 
-	//sets the lineItems array in the order object
-	$order->setLineItems($lineItems);
+// 	//sets the lineItems array in the order object
+// 	$order->setLineItems($lineItems);
 
-	$checkout = new \SquareConnect\Model\CreateCheckoutRequest();
+// 	$checkout = new \SquareConnect\Model\CreateCheckoutRequest();
 
-	$checkout->setIdempotencyKey(uniqid()); //uniqid() generates a random string.
-	$checkout->setOrder($order); //this is the order we created in the previous step
+// 	$checkout->setIdempotencyKey(uniqid()); //uniqid() generates a random string.
+// 	$checkout->setOrder($order); //this is the order we created in the previous step
 
-	try {
-	    $result = $checkoutClient->createCheckout(
-	      $locationId,
-	      $checkout
-	    );
-    //Save the checkout ID for verifying transactions
-	    $checkoutId = $result->getCheckout()->getId();
-	    //Get the checkout URL that opens the checkout page.
-	    $checkoutUrl = $result->getCheckout()->getCheckoutPageUrl();
-    	// print_r('Complete your transaction: ' + $checkoutUrl);
-    	// echo(json_encode($checkoutUrl));
+// 	try {
+// 	    $result = $checkoutClient->createCheckout(
+// 	      $locationId,
+// 	      $checkout
+// 	    );
+//     //Save the checkout ID for verifying transactions
+// 	    $checkoutId = $result->getCheckout()->getId();
+// 	    //Get the checkout URL that opens the checkout page.
+// 	    $checkoutUrl = $result->getCheckout()->getCheckoutPageUrl();
+//     	// print_r('Complete your transaction: ' + $checkoutUrl);
+//     	// echo(json_encode($checkoutUrl));
 
-    	header( 'Location:'. $checkoutUrl) ;
-	} catch (Exception $e) {
-    echo 'Exception when calling CheckoutApi->createCheckout: ', $e->getMessage(), PHP_EOL;
-	}
+//     	header( 'Location:'. $checkoutUrl) ;
+// 	} catch (Exception $e) {
+//     echo 'Exception when calling CheckoutApi->createCheckout: ', $e->getMessage(), PHP_EOL;
+// 	}
 
 ?>
